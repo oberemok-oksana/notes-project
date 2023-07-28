232459bccdac40be3634b84d6846130c34af4519
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-28 14:43:48 +0700

    implement showing a note creating card by clicking

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const createNoteBtn = document.getElementById("create-note");
+  const noteCreatingCard = document.getElementById("note-creating-card");
+
+  createNoteBtn.addEventListener("click", () => {
+    noteCreatingCard.classList.toggle("visible");
+  });
+});
