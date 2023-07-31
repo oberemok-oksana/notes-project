03a602b885216493576a31015153358f946e3be0
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 13:04:20 +0700

    create new lib, relocate createDate there

---
+++ src/lib.js
@@ -0,0 +1,22 @@
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const createDate = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  return `${MONTHS[month]} ${day}, ${year}`;
+};
