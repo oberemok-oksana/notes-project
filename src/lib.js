34996a20c748f6378b94ed0649338c8be862a762
Oksana Oberemok <Pylypenkok@gmail.com>
2023-07-31 13:33:08 +0700

    move deleteNote from main.js

--- src/lib.js
+++ src/lib.js
@@ -30,3 +30,8 @@ export const getArchivedNotes = (data) => {
   const filtered = data.filter((note) => !note.active);
   return filtered;
 };
+
+export const deleteNote = (data, id) => {
+  const filteredData = data.filter((note) => note.id !== id);
+  return filteredData;
+};
